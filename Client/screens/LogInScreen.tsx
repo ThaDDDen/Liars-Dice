30381ef796b4c19cd4fe47a2786bcd32e3893d15
Tommy Zarén <85tomzar@skola.boras.se>
2022-12-10 21:04:40 +0100

    created initial screens

---
+++ Client/screens/LogInScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const LogInScreen = () => {
+  return (
+    <View>
+      <Text>LogIn</Text>
+    </View>
+  );
+};
+
+export default LogInScreen;
