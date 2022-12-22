9ed91c8c186f93cf37623e8d62740649b09c27fe
Tommy Zarén <85tomzar@skola.boras.se>
2022-12-22 12:29:33 +0100

    created and implemented "content-card" component

---
+++ Client/components/profile/Statistics.tsx
@@ -0,0 +1,32 @@
+import { Entypo, FontAwesome5 } from "@expo/vector-icons";
+import React from "react";
+import { View } from "react-native";
+import { Text, useTheme } from "react-native-paper";
+
+const Statistics = () => {
+  const { colors } = useTheme();
+  return (
+    <>
+      <View style={{ marginLeft: 10, marginTop: 10, flexDirection: "row", alignItems: "baseline" }}>
+        <Entypo name="trophy" size={30} color="#FFFF47" />
+        <Text style={{ marginLeft: 10, marginRight: "auto" }} variant="headlineSmall">
+          Games won
+        </Text>
+        <Text style={{ marginRight: 15 }} variant="headlineSmall">
+          4
+        </Text>
+      </View>
+      <View style={{ marginLeft: 10, marginTop: 10, paddingLeft: 3, flexDirection: "row", alignItems: "baseline" }}>
+        <FontAwesome5 name="dice-five" size={30} color="#ffafcc" />
+        <Text style={{ marginLeft: 10, marginRight: "auto" }} variant="headlineSmall">
+          Games played
+        </Text>
+        <Text style={{ marginRight: 15 }} variant="headlineSmall">
+          18
+        </Text>
+      </View>
+    </>
+  );
+};
+
+export default Statistics;
