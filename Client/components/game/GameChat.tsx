73ac37c3c8d3a36cfd32b813cf6d3d658dc12df2
osci-the-orca <88oscste@skola.boras.se>
2022-12-25 19:35:53 +0100

    broke out gamechat and table to its own components

---
+++ Client/components/game/GameChat.tsx
@@ -0,0 +1,46 @@
+import React, { useRef } from "react";
+import { ScrollView, View } from "react-native";
+import { useTheme } from "react-native-paper";
+import styled from "styled-components/native";
+import { useGame } from "../../contexts/GameContext";
+import { useUser } from "../../contexts/UserContext";
+import ChatMessage from "../Lobby/ChatMessage";
+import MessageForm from "../Lobby/MessageForm";
+
+const GameChat = () => {
+  const { colors } = useTheme();
+  const { gameMessages } = useUser();
+  const { game } = useGame();
+
+  const scrollViewRef = useRef<ScrollView | null>(null);
+
+  const gameMessagesJsx = gameMessages.map((userMessage, index) => (
+    <View key={index}>
+      <ChatMessage userMessage={userMessage} latestMessage={gameMessages.length === index + 1} />
+    </View>
+  ));
+
+  return (
+    <ChatContainer>
+      <ChatWindow ref={scrollViewRef} onContentSizeChange={() => scrollViewRef.current?.scrollToEnd({ animated: true })} bg={colors.surfaceVariant}>
+        {gameMessagesJsx}
+      </ChatWindow>
+      <MessageForm chatName={game.gameName} />
+    </ChatContainer>
+  );
+};
+
+export default GameChat;
+
+const ChatWindow = styled.ScrollView<{ bg: string }>`
+  background-color: ${({ bg }) => bg};
+  height: 200px;
+  border-radius: 10px;
+  padding: 10px;
+`;
+
+const ChatContainer = styled.View`
+  width: 100%;
+  flex: 1;
+  padding: 10px;
+`;
--- Client/components/game/GameLobby.tsx
+++ Client/components/game/GameLobby.tsx
@@ -1,21 +1,17 @@
 import React, { useRef } from "react";
-import { Dimensions, ImageBackground, Pressable, ScrollView, View } from "react-native";
 import { Modalize } from "react-native-modalize";
 import { Text, useTheme } from "react-native-paper";
 import styled from "styled-components/native";
-import table from "../../assets/images/table.png";
 import { useConnection } from "../../contexts/ConnectionContext";
 import { useGame } from "../../contexts/GameContext";
 import { useUser } from "../../contexts/UserContext";
-import { EIGHT_SEAT_TABLE, FOUR_SEAT_TABLE, INVOKE_ROLL_DICE, SIX_SEAT_TABLE } from "../../utils/constants";
+import { INVOKE_ROLL_DICE } from "../../utils/constants";
 import Background from "../layout/Background";
 import Button from "../layout/Button";
-import ChatMessage from "../Lobby/ChatMessage";
-import MessageForm from "../Lobby/MessageForm";
 import OnlineUserCard from "../Lobby/OnlineUserCard";
-import UserAvatar from "../Lobby/UserAvatar";
+import GameChat from "./GameChat";
 import GameHeader from "./GameHeader";
-import PlayerCard from "./PlayerCard";
+import Table from "./Table";
 import UserHand from "./UserHand";
 
 const GameLobby = () => {
@@ -23,9 +19,7 @@ const GameLobby = () => {
   const { currentUser } = useUser();
   const { connection, connectedUsers } = useConnection();
   const { colors } = useTheme();
-  const { gameMessages } = useUser();
 
-  const scrollViewRef = useRef<ScrollView | null>(null);
   const chatModalize = useRef<Modalize>(null);
   const usersOnlineModalize = useRef<Modalize>(null);
 
@@ -37,67 +31,10 @@ const GameLobby = () => {
     usersOnlineModalize.current?.open();
   };
 
-  // STÄDA KODEN FÖR HELVETE TOMMY! DET SER UT SOM FAAN
-  const tableHeight = game.playerCount > 6 ? 0.6 : game.playerCount > 4 ? 0.4 : 0.35;
-
-  const playersInGameLobby = game.players.map((player, index) => {
-    return (
-      <View
-        key={index}
-        style={game.playerCount > 6 ? EIGHT_SEAT_TABLE[index] : game.playerCount > 4 ? SIX_SEAT_TABLE[index] : FOUR_SEAT_TABLE[index]}
-      >
-        <PlayerCard player={player} />
-      </View>
-    );
-  });
-
-  const placeHolderAvatars = Array.from({ length: game.playerCount - game.players.length }, (value, index) => (
-    <Pressable
-      key={index}
-      style={
-        game.playerCount > 6
-          ? EIGHT_SEAT_TABLE[index + game.players.length]
-          : game.playerCount > 4
-          ? SIX_SEAT_TABLE[index + game.players.length]
-          : FOUR_SEAT_TABLE[index + game.players.length]
-      }
-      onPress={() => openOnlineUsersModal()}
-    >
-      <UserAvatar size={50} avatarCode="PlaceHolder" />
-    </Pressable>
-  ));
-
-  const gameMessagesJsx = gameMessages.map((userMessage, index) => (
-    <View key={index}>
-      <ChatMessage userMessage={userMessage} latestMessage={gameMessages.length === index + 1} />
-    </View>
-  ));
-
   return (
     <Background>
       <GameHeader openChatModal={openChatModal} />
-
-      <Table>
-        <TableBackground
-          source={table}
-          resizeMode="cover"
-          width={Dimensions.get("window").width * 0.7}
-          height={Dimensions.get("window").height * tableHeight}
-        />
-        <TableOverlay width={Dimensions.get("window").width * 0.7} height={Dimensions.get("window").height * tableHeight}>
-          {playersInGameLobby}
-          {placeHolderAvatars}
-          <View style={{ backgroundColor: "#ffffff33", borderRadius: 5, padding: 5, borderWidth: 2, borderColor: "black" }}>
-            <Text style={{ color: "black" }} variant="titleMedium">
-              Round 22
-            </Text>
-            <Text style={{ color: "black" }}>Dice left 4/26</Text>
-            <Text style={{ color: "black" }}>thad bet 2 x 5</Text>
-            <Text style={{ color: "black" }}>It's oscars turn!</Text>
-          </View>
-        </TableOverlay>
-      </Table>
-
+      <Table openOnlineUsersModal={openOnlineUsersModal} />
       <GameBar>
         <Button
           title={"roll"}
@@ -110,26 +47,14 @@ const GameLobby = () => {
         <UserHand dice={game.players.find((x) => x.userName === currentUser.userName)?.dice} />
       </GameBar>
 
-      <Modalize ref={chatModalize} rootStyle={{}} modalStyle={{ backgroundColor: colors.surface, padding: 5 }} adjustToContentHeight>
-        {/* TODO: Break out chat to its own component */}
-        <ChatContainer>
-          <ChatWindow
-            ref={scrollViewRef}
-            onContentSizeChange={() => scrollViewRef.current?.scrollToEnd({ animated: true })}
-            bg={colors.surfaceVariant}
-          >
-            {gameMessagesJsx}
-          </ChatWindow>
-        </ChatContainer>
-        <MessageForm chatName={game.gameName} />
+      <Modalize ref={chatModalize} rootStyle={{}} modalStyle={{ backgroundColor: colors.surface }} adjustToContentHeight>
+        <GameChat />
       </Modalize>
 
       <Modalize ref={usersOnlineModalize} rootStyle={{}} modalStyle={{ backgroundColor: colors.surface, padding: 5 }} adjustToContentHeight>
         <OnlinePlayersText variant="titleMedium">Invite players:</OnlinePlayersText>
         {connectedUsers.map((user, index) => (
-          <View key={index}>
-            <OnlineUserCard userConnection={user} closeModal={() => usersOnlineModalize.current?.close()} />
-          </View>
+          <OnlineUserCard key={index} userConnection={user} closeModal={() => usersOnlineModalize.current?.close()} />
         ))}
       </Modalize>
     </Background>
@@ -138,49 +63,11 @@ const GameLobby = () => {
 
 export default GameLobby;
 
-const Table = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const TableBackground = styled(ImageBackground)<{ width: number; height: number }>`
-  width: ${({ width }) => width}px;
-  height: ${({ height }) => height}px;
-  justify-content: center;
-  border-radius: 1000px;
-  border-width: 20px;
-  overflow: hidden;
-  border-color: #513624;
-`;
-
-const TableOverlay = styled.View<{ width: number; height: number }>`
-  position: absolute;
-  width: ${({ width }) => width}px;
-  height: ${({ height }) => height}px;
-  align-items: center;
-  justify-content: center;
-`;
-
 const GameBar = styled.View`
   align-items: center;
   justify-content: center;
   flex-direction: row;
   justify-content: space-around;
-`;
-
-const ChatWindow = styled.ScrollView<{ bg: string }>`
-  background-color: ${({ bg }) => bg};
-  /* flex: 1; */
-  height: 200px;
-  padding: 10px;
-  border-radius: 10px;
-`;
-
-const ChatContainer = styled.View`
-  /* flex: 1; */
-  width: 100%;
-  padding: 10px;
 `;
 
 const OnlinePlayersText = styled(Text)`
