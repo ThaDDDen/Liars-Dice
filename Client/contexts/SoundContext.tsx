85b3ed542b2d4074a38c5dd073df86ff4cc55259
Tommy Zarén <85tomzar@skola.boras.se>
2022-12-29 12:04:49 +0100

    added sound context and sample sound

---
+++ Client/contexts/SoundContext.tsx
@@ -0,0 +1,51 @@
+import { Audio } from "expo-av";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import AllSounds, { SoundObject } from "../utils/sounds";
+
+interface SoundContextValue {
+  allSounds: SoundObject[];
+  playWinnerSound: () => void;
+}
+
+const SoundContext = createContext<SoundContextValue>({
+  allSounds: [],
+  playWinnerSound: () => console.warn("No provier found."),
+});
+
+interface Props {
+  children: ReactNode;
+}
+
+function SoundProvider({ children }: Props) {
+  const [allSounds, setAllSounds] = useState<SoundObject[]>(AllSounds);
+  const [music, setMusic] = useState<Audio.Sound>();
+
+  useEffect(() => {
+    return music
+      ? () => {
+          music.unloadAsync();
+        }
+      : undefined;
+  }, [music]);
+
+  const playWinnerSound = async () => {
+    const { sound, status } = await Audio.Sound.createAsync(allSounds[0].sound);
+    setMusic(sound);
+    await sound.playAsync();
+  };
+
+  return (
+    <SoundContext.Provider
+      value={{
+        allSounds,
+        playWinnerSound,
+      }}
+    >
+      {children}
+    </SoundContext.Provider>
+  );
+}
+
+export const useSound = () => useContext(SoundContext);
+
+export default SoundProvider;
