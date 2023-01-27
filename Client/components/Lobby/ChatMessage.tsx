7ccdf253753fd61492a1c95d27418d4d33bbb8a0
osci-the-orca <88oscste@skola.boras.se>
2023-01-27 14:29:59 +0100

    fixed bug with bodySmall Text

--- Client/components/Lobby/ChatMessage.tsx
+++ Client/components/Lobby/ChatMessage.tsx
@@ -14,30 +14,28 @@ interface Props {
 const ChatMessage = ({ userMessage, latestMessage, lastSender }: Props) => {
   const { colors } = useTheme();
   return (
-    <>
-      <ChatMessageContainer latestMessage={latestMessage} lastSender={lastSender}>
+    <ChatMessageContainer latestMessage={latestMessage} lastSender={lastSender}>
+      {!lastSender && (
+        <AvatarContainer>
+          <UserAvatar size={40} avatarCode={userMessage.user.avatarCode} />
+        </AvatarContainer>
+      )}
+      <View style={{ marginLeft: 10 }}>
         {!lastSender && (
-          <AvatarContainer>
-            <UserAvatar size={40} avatarCode={userMessage.user.avatarCode} />
-          </AvatarContainer>
-        )}
-        <View style={{ marginLeft: 10, flexGrow: 1, flex: 1 }}>
-          {!lastSender && (
-            <View style={{ flexDirection: "row", alignItems: "baseline" }}>
-              <Text variant="titleMedium" style={{ marginRight: 10 }}>
-                {userMessage.user.userName}
-              </Text>
-              <Text variant="bodySmall" style={{ fontStyle: "italic", color: "gray" }}>
-                {userMessage.time}
-              </Text>
-            </View>
-          )}
-          <View style={{ marginLeft: lastSender ? 45 : 0 }}>
-            <Text variant="bodyMedium">{userMessage.message}</Text>
+          <View style={{ flexDirection: "row", alignItems: "baseline" }}>
+            <Text variant="titleMedium" style={{ marginRight: 10 }}>
+              {userMessage.user.userName}
+            </Text>
+            <Text variant="titleSmall" style={{ fontSize: 12, fontStyle: "italic", color: colors.outline }}>
+              {userMessage.time}
+            </Text>
           </View>
+        )}
+        <View style={{ marginLeft: lastSender ? 45 : 0 }}>
+          <Text variant="bodyMedium">{userMessage.message}</Text>
         </View>
-      </ChatMessageContainer>
-    </>
+      </View>
+    </ChatMessageContainer>
   );
 };
 
@@ -45,7 +43,6 @@ export default ChatMessage;
 
 const AvatarContainer = styled.View`
   padding: 5px 5px 0 0;
-  align-self: flex-start;
 `;
 
 const ChatMessageContainer = styled.View<{ latestMessage: boolean; lastSender: boolean }>`
