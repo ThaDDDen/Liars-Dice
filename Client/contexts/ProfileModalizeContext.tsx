1c8d9b1ce79c589a4d0876aff99bbce5dd15f9bc
Tommy Zarén <85tomzar@skola.boras.se>
2023-03-04 10:27:47 +0100

    added context for fetching profile

---
+++ Client/contexts/ProfileModalizeContext.tsx
@@ -0,0 +1,204 @@
+import React, { createContext, ReactNode, useContext, useEffect, useRef, useState } from "react";
+import { View } from "react-native";
+import { Modalize } from "react-native-modalize";
+import { Surface, Text, Tooltip, useTheme } from "react-native-paper";
+import styled from "styled-components/native";
+import ContentCard from "../components/layout/ContentCard";
+import ProfileAvatar from "../components/profile/ProfileAvatar";
+import Statistics from "../components/profile/Statistics";
+import { Profile, User } from "../types/types";
+import { initialProfileState, INVOKE_INVITE_PLAYER, INVOKE_KICK_PLAYER } from "../utils/constants";
+import { useConnection } from "./ConnectionContext";
+import { initialGameState, useGame } from "./GameContext";
+import { initialUserState, useUser } from "./UserContext";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface ProfileModalizeContext {
+  loadedProfile: Profile;
+  setLoadedProfile: React.Dispatch<React.SetStateAction<Profile>>;
+  setFetchUser: React.Dispatch<React.SetStateAction<User>>;
+}
+
+const ProfileModalizeContext = createContext<ProfileModalizeContext>({
+  loadedProfile: {} as Profile,
+  setLoadedProfile: () => console.warn("No provider found."),
+  setFetchUser: () => console.warn("No provider found."),
+});
+
+const ProfileModalizeProvider = ({ children }: Props) => {
+  const [loadedProfile, setLoadedProfile] = useState(initialProfileState);
+  const { colors } = useTheme();
+  const { currentUser } = useUser();
+  const { game } = useGame();
+  const { connection } = useConnection();
+  const [fetchingProfile, setFetchingProfile] = useState(false);
+  const [fetchUser, setFetchUser] = useState<User>(initialUserState);
+
+  const profileModalize = useRef<Modalize>(null);
+
+  useEffect(() => {
+    if (fetchUser !== initialUserState) {
+      setFetchingProfile(true);
+      const fetchStatistics = async () => {
+        const response = await fetch(`http://192.168.0.4:5141/api/Statistics/GetStatisticsByProfileId/${fetchUser.id}`);
+        const jsonData = await response.json();
+
+        setLoadedProfile({
+          id: fetchUser.id,
+          userName: fetchUser.userName,
+          avatarCode: fetchUser.avatarCode,
+          statistics: jsonData,
+        });
+
+        setFetchingProfile(false);
+      };
+
+      fetchStatistics();
+    }
+  }, [fetchUser]);
+
+  useEffect(() => {
+    if (loadedProfile !== initialProfileState) {
+      openProfileModalize();
+    }
+  }, [loadedProfile]);
+
+  const unloadProfile = () => {
+    setLoadedProfile(initialProfileState);
+  };
+
+  const handleInvitePlayer = () => {
+    connection.invoke(INVOKE_INVITE_PLAYER, currentUser, loadedProfile.userName);
+  };
+
+  const handleKickPlayer = () => {
+    connection.invoke(INVOKE_KICK_PLAYER, loadedProfile.id);
+  };
+
+  const openProfileModalize = () => {
+    profileModalize.current?.open();
+  };
+
+  const closeProfileModalize = () => {
+    profileModalize.current?.close();
+  };
+
+  const handleLeftButtonPress = () => {
+    if (currentUser.userName !== loadedProfile.userName && !game.players.find((p) => p.userName === loadedProfile.userName)) {
+      handleInvitePlayer();
+      closeProfileModalize();
+    } else {
+      handleKickPlayer();
+      closeProfileModalize();
+    }
+  };
+
+  return (
+    <ProfileModalizeContext.Provider value={{ loadedProfile, setLoadedProfile, setFetchUser }}>
+      <>
+        {children}
+        {loadedProfile !== initialProfileState && (
+          <Modalize
+            onClose={() => {
+              unloadProfile();
+              setFetchUser(initialUserState);
+            }}
+            ref={profileModalize}
+            rootStyle={{}}
+            modalStyle={{ shadowColor: "transparent", backgroundColor: "transparent" }}
+            withHandle={false}
+            adjustToContentHeight
+          >
+            {loadedProfile !== initialProfileState && (
+              <ModalContent>
+                <ProfileAvatar profile={loadedProfile} />
+                <ModalBackground backgroundColor={colors.primaryContainer}>
+                  <TopButtons>
+                    {game === initialGameState ? (
+                      <View style={{ flex: 1 }}>
+                        <Tooltip enterTouchDelay={0} title="You need to create a game in order to invite players">
+                          <LeftButton backgroundColor={colors.surface}>
+                            <LeftButtonText textColor={"grey"}>INVITE</LeftButtonText>
+                          </LeftButton>
+                        </Tooltip>
+                      </View>
+                    ) : (
+                      <LeftButton backgroundColor={colors.surface} onPress={() => handleLeftButtonPress()}>
+                        <LeftButtonText textColor={colors.onPrimary}>
+                          {game !== initialGameState &&
+                          currentUser.userName !== loadedProfile.userName &&
+                          !game.players.find((p) => p.userName === loadedProfile.userName)
+                            ? "INVITE"
+                            : "KICK"}
+                        </LeftButtonText>
+                      </LeftButton>
+                    )}
+                    <RightButton backgroundColor={colors.secondary}>
+                      <RightButtonText>ADD FRIEND</RightButtonText>
+                    </RightButton>
+                  </TopButtons>
+                  <ContentCard borderColor="#161545" label="rolls">
+                    <Statistics statistics={loadedProfile.statistics} />
+                  </ContentCard>
+                </ModalBackground>
+              </ModalContent>
+            )}
+          </Modalize>
+        )}
+      </>
+    </ProfileModalizeContext.Provider>
+  );
+};
+
+export const useProfileModalize = () => useContext(ProfileModalizeContext);
+
+export default ProfileModalizeProvider;
+
+const ModalContent = styled.View`
+  flex: 1;
+`;
+
+const ModalBackground = styled.View<{ backgroundColor: string }>`
+  flex: 1;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  margin-top: 50px;
+  border-top-left-radius: 24px;
+  border-top-right-radius: 24px;
+`;
+
+const TopButtons = styled(Surface)`
+  border-radius: 15px;
+  margin: -40px 10px 5px 10px;
+  flex-direction: row;
+`;
+
+const LeftButton = styled.Pressable<{ backgroundColor: string }>`
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  flex: 1;
+  border-radius: 15px;
+  align-items: center;
+  padding: 5px 10px 10px 10px;
+`;
+
+const RightButton = styled.Pressable<{ backgroundColor: string }>`
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  flex: 1;
+  border-radius: 15px;
+  align-items: center;
+  padding: 5px 10px 10px 10px;
+  border-top-left-radius: 0px;
+`;
+const LeftButtonText = styled(Text)<{ textColor: string }>`
+  font-size: 26px;
+  font-family: "Manrope-Bold";
+
+  color: ${({ textColor }) => textColor};
+`;
+
+const RightButtonText = styled(Text)`
+  font-size: 26px;
+  font-family: "Manrope-Bold";
+`;
--- Client/screens/GameHomeScreen.tsx
+++ Client/screens/GameHomeScreen.tsx
@@ -1,141 +1,69 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { useRef } from "react";
-import { Pressable, View } from "react-native";
-import { Modalize } from "react-native-modalize";
-import { Button, Surface, Text, useTheme } from "react-native-paper";
+import React from "react";
+import { Text, useTheme } from "react-native-paper";
 import styled from "styled-components/native";
 import Background from "../components/layout/Background";
-import ContentCard from "../components/layout/ContentCard";
 import Logo from "../components/layout/Logo";
-import ProfileAvatar from "../components/profile/ProfileAvatar";
-import Statistics from "../components/profile/Statistics";
-import { useUser } from "../contexts/UserContext";
 import { GameStackParams } from "../navigation/GameStackNavigator";
 
 export type GameHomeNavProps = NativeStackScreenProps<GameStackParams>;
 
 const GameHomeScreen = ({ navigation }: GameHomeNavProps) => {
   const { colors } = useTheme();
-  const { currentUser } = useUser();
-
-  const profileModalize = useRef<Modalize>(null);
-
-  const openModal = () => {
-    profileModalize.current?.open();
-  };
 
   return (
     <Background>
       <Logo size={"medium"} />
-      {/* <ButtonContainer>
-        <Button title={"create game"} mode={"contained"} styles={{ marginBottom: 20 }} onPress={() => navigation.navigate("CreateGameScreen")} />
-        <Button title={"join game"} mode={"contained"} onPress={() => navigation.navigate("JoinGameScreen")} />
-      </ButtonContainer> */}
-
-      <Surface style={{ borderRadius: 15, marginBottom: 5, marginTop: 40, marginHorizontal: 10, flexDirection: "row" }}>
-        <View
-          style={{
-            width: "50%",
-            alignItems: "center",
-          }}
-        >
-          <Pressable
-            onPress={() => navigation.navigate("CreateGameScreen")}
-            style={{
-              backgroundColor: colors.surface,
-              width: "100%",
-              borderRadius: 15,
-              alignItems: "center",
-              padding: 10,
-              paddingTop: 5,
-            }}
-          >
-            <View style={{ alignItems: "center" }}>
+      <ButtonContainer backgroundColor={colors.surface}>
+        <ButtonWrapper>
+          <Button onPress={() => navigation.navigate("CreateGameScreen")} backgroundColor={colors.surface}>
+            <ButtonTextContainer>
               <Text style={{ fontFamily: "Manrope-Regular", fontSize: 30 }}>CREATE</Text>
               <Text style={{ fontFamily: "Manrope-SemiBold", fontSize: 16 }}>GAME</Text>
-            </View>
-          </Pressable>
-        </View>
-        <View
-          style={{
-            width: "50%",
-            alignItems: "center",
-          }}
-        >
-          <Pressable
+            </ButtonTextContainer>
+          </Button>
+        </ButtonWrapper>
+        <ButtonWrapper>
+          <Button
             onPress={() => navigation.navigate("JoinGameScreen")}
             style={{
-              backgroundColor: colors.secondary,
-              width: "100%",
-              borderRadius: 15,
-              alignItems: "center",
-              padding: 10,
-              paddingTop: 5,
               borderTopLeftRadius: 0,
             }}
+            backgroundColor={colors.secondary}
           >
-            <View style={{ alignItems: "center" }}>
+            <ButtonTextContainer>
               <Text style={{ fontFamily: "Manrope-Regular", fontSize: 30 }}>JOIN</Text>
               <Text style={{ fontFamily: "Manrope-SemiBold", fontSize: 16 }}>GAME</Text>
-            </View>
-          </Pressable>
-        </View>
-      </Surface>
-      <Button onPress={() => openModal()}>Hello</Button>
-      <Modalize ref={profileModalize} rootStyle={{}} modalStyle={{ backgroundColor: "transparent" }} withHandle={false} adjustToContentHeight>
-        <View style={{ flex: 1 }}>
-          <ProfileAvatar />
-          <View style={{ flex: 1, backgroundColor: "#161545", marginTop: 50, borderTopLeftRadius: 24, borderTopRightRadius: 24 }}>
-            <Surface style={{ borderRadius: 15, marginBottom: 5, marginTop: -40, marginHorizontal: 10, flexDirection: "row" }}>
-              <View
-                style={{
-                  backgroundColor: colors.surface,
-                  width: "50%",
-                  // margin: -10,
-                  borderRadius: 15,
-                  alignItems: "center",
-                  padding: 10,
-                  paddingTop: 5,
-                }}
-              >
-                <Text style={{ fontFamily: "Manrope-Regular", fontSize: 35 }}>{currentUser.statistics.gamesPlayed}</Text>
-                <Text style={{ fontFamily: "Manrope-SemiBold", fontSize: 16 }}>GAMES PLAYED</Text>
-              </View>
-              <View
-                style={{
-                  backgroundColor: colors.secondary,
-                  width: "50%",
-                  // margin: -10,
-                  borderRadius: 15,
-                  alignItems: "center",
-                  padding: 10,
-                  paddingTop: 5,
-                  borderTopLeftRadius: 0,
-                }}
-              >
-                <Text style={{ fontFamily: "Manrope-Regular", fontSize: 35 }}>{currentUser.statistics.gamesWon}</Text>
-                <Text style={{ fontFamily: "Manrope-SemiBold", fontSize: 16 }}>GAMES WON</Text>
-              </View>
-            </Surface>
-            <ContentCard borderColor="#161545" label="rolls">
-              <Statistics statistics={currentUser.statistics} />
-            </ContentCard>
-            {/* <ContentCard title="Statistics">
-            <Statistics />
-          </ContentCard> */}
-            {/* <Button title={"log out"} mode={"contained"} onPress={() => handleLogout()} styles={{ marginTop: "auto", borderRadius: 5 }} /> */}
-          </View>
-        </View>
-      </Modalize>
+            </ButtonTextContainer>
+          </Button>
+        </ButtonWrapper>
+      </ButtonContainer>
     </Background>
   );
 };
 
 export default GameHomeScreen;
 
-const LeftButton = styled.View``;
+const ButtonContainer = styled.View<{ backgroundColor: string }>`
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  border-radius: 15px;
+  margin: 40px 10px 5px 10px;
+  flex-direction: row;
+`;
 
-const ButtonContainer = styled.View`
-  padding: 10px 110px;
-  margin-top: 20px;
+const ButtonWrapper = styled.View`
+  width: 50%;
+  align-items: center;
+`;
+
+const Button = styled.Pressable<{ backgroundColor: string }>`
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  width: 100%;
+  border-radius: 15px;
+  align-items: center;
+  padding: 5px 10px 10px 10px;
+`;
+
+const ButtonTextContainer = styled.View`
+  align-items: center;
 `;
