6afa9a1381f07985c5ae134d2ee76f77f20cc125
osci-the-orca <88oscste@skola.boras.se>
2023-04-04 19:34:35 +0200

    conditinalrendering for onboarding or home

--- Client/screens/HomeScreen.tsx
+++ Client/screens/HomeScreen.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { Pressable } from "react-native";
 import { Text } from "react-native-paper";
 import Home from "../components/Home";
+import OnBoarding from "../components/onboarding/OnBoarding";
 import { useUser } from "../contexts/UserContext";
 import { BottomTabStackParams } from "../navigation/BottomTabStackNavigator";
 import { RootStackParams } from "../navigation/RootStackNavigator";
@@ -12,10 +13,11 @@ import { RootStackParams } from "../navigation/RootStackNavigator";
 export type HomeNavProps = CompositeScreenProps<BottomTabScreenProps<RootStackParams>, NativeStackScreenProps<BottomTabStackParams>>;
 
 const HomeScreen = ({ navigation, route }: HomeNavProps) => {
-  // const [firstVisit, setFirstVisit] = useAsyncStorage("firstVisit", true);
-  const { firstVisit, setFirstVisit } = useUser();
+  const { setFirstVisit, firstVisit } = useUser();
 
-  return (
+  return firstVisit ? (
+    <OnBoarding />
+  ) : (
     <>
       <Home navigation={navigation} route={route} />
       <Pressable onPress={() => setFirstVisit(true)}>
